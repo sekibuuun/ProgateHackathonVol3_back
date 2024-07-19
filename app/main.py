1750683a4ba9c6c5123c300e8a7d05ed833ddd35
sekibuuun <akaunto.no.001@gmail.com>
2024-07-19 18:19:45 +0900

    feat: user情報の取得

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
 
 app = FastAPI()
 
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    user = supabase.table("test_user").select("*").eq("user_id", user_id).execute()
+    return user.data
