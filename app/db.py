9a7872a272865863602825f838b6fecbbcc6142e
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-07-20 19:40:38 +0900

    dotenvを使うように変更

--- app/db.py
+++ app/db.py
@@ -1,6 +1,9 @@
 # app/db.py
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
