86d905748abd932523dc396ef780d6f7bb873219
sekibuuun <akaunto.no.001@gmail.com>
2024-07-21 14:48:44 +0900

    fix: フレンドを双方向に登録

--- app/api/friend.py
+++ app/api/friend.py
@@ -13,16 +13,18 @@ def read_friends(user_id: str):
     friend_user_response = supabase.table("friend").select("user_id").eq("friend_id", user_id).execute()
 
     # レスポンスデータをリストに追加
-    all_friend_response = []
-    all_friend_response.append(user_friend_response.data)
-    all_friend_response.append(friend_user_response.data)
-
-    # フラットなリストに変換する
-    friend_user_ids = [item['user_id'] for sublist in all_friend_response for item in sublist]
+    friend_user_ids = []
+    friend_user_ids.append([item['friend_id'] for item in user_friend_response.data])
+    friend_user_ids.append([item['user_id'] for item in friend_user_response.data])
 
     logging.getLogger("uvicorn").info(friend_user_ids)
 
-    if not all_friend_response:
+    # フラットなリストに変換する
+    friend_user_ids = [item for sublist in friend_user_ids for item in sublist]
+
+    logging.getLogger("uvicorn").info(friend_user_ids)
+
+    if not friend_user_ids:
         raise HTTPException(status_code=404, detail="Friends not found")
     
     # 友達の user_id に基づいて個別にデータを取得
